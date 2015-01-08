dc545cb724716b45fe6f6cd5bb5b452f915b6c1c
mrliptontea <rayman17@gmail.com>
2015-01-08 09:44:36 +0100

    initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,43 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON("package.json"),
+    banner: "/*! <%= pkg.title || pkg.name %> - " +
+      "<%= grunt.template.today('yyyy-mm-dd') %>\n" +
+      " * Copyright (c) <%= grunt.template.today('yyyy') %> <%= pkg.author %>\n */\n\n",
+
+    sass: {
+      dist: {
+        options: {
+          style: 'compressed'
+        },
+        files: {
+          'stylesheets/application.css': 'sass/application.scss'
+        }
+      },
+      redmine_backlogs: {
+        options: {
+          style: 'compressed'
+        },
+        files: {
+          'plugins/redmine_backlogs/global.css':         'sass/plugins/redmine_backlogs/global.scss',
+          'plugins/redmine_backlogs/master_backlog.css': 'sass/plugins/redmine_backlogs/master_backlog.scss',
+          'plugins/redmine_backlogs/statistics.css':     'sass/plugins/redmine_backlogs/statistics.scss',
+          'plugins/redmine_backlogs/taskboard.css':      'sass/plugins/redmine_backlogs/taskboard.scss'
+        }
+      }
+    },
+
+    watch: {
+      css: {
+        files: ['sass/**/*.scss'],
+        tasks: ['sass'],
+      },
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  grunt.registerTask('default', ['sass']);
+};
---
+++ README.md
@@ -0,0 +1,31 @@
+# PurpleMine 2
+
+A free Redmine theme for modern browsers.
+
+![Screenshot](https://github.com/HolonGlobe/PurpleMine2/raw/master/screenshots/issues-list.png)
+
+Compatible with Redmine 2.5+ and browsers: IE8+, latest Firefox and Google Chrome (others where not tested).
+
+It's written in [SASS] (SCSS to be precise). It uses [normalize.css] and benefits from some parts of [Bootstrap][bootstrap-sass] like mixins, structure and stuff.
+
+## Main features
+
+* Bigger, easier to read fonts
+* Wiki content with Github-ish style
+* Sidebar moved to the left for better ergonomy (it's customizable via `_variables.scss`)
+* Coloring trackers links (on lists, issue pages and even in the wiki content)
+* Highlighting issues priority on the list and on the issue page
+
+## How install it
+
+To install PurpleMine, just download [.zip](https://github.com/HolonGlobe/PurpleMine2/archive/master.zip) and unpack it to your Redmine's `/public/themes` folder.
+
+Then go to Redmine > Administration > Settings > Display and select PurpleMine2 from the list and save the changes.
+
+## License
+
+[WTFPL](http://www.wtfpl.net/)
+
+[SASS]: http://sass-lang.com/
+[normalize.css]: https://github.com/necolas/normalize.css
+[bootstrap-sass]: https://github.com/twbs/bootstrap-sass
