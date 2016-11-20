beeff247a47dd40fdf02861487df9542ab28c3a7
mrliptontea <rayman17@gmail.com>
2016-11-20 22:38:49 +0000

    Extend scss linter settings
    - Enable DeclarationOrder rule
    - Change declaration order in scss
    - Enable VendorPrefix rule
    - Add Autoprefixer via Postcss to css task
    - Move scss-lint comments in-line

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,4 @@
 module.exports = function(grunt) {
-
   grunt.initConfig({
     src: "src/",
 
@@ -8,33 +7,44 @@ module.exports = function(grunt) {
         sourceMap: false,
         outputStyle: 'compressed'
       },
-      dist: {
+
+      theme: {
         files: {
           'stylesheets/application.css': '<%= src %>sass/application.scss'
         }
       },
-      redmine_backlogs: {
-        files: {
-          'plugins/redmine_backlogs/global.css':
-            '<%= src %>sass/plugins/redmine_backlogs/global.scss',
-          'plugins/redmine_backlogs/master_backlog.css':
-            '<%= src %>sass/plugins/redmine_backlogs/master_backlog.scss',
-          'plugins/redmine_backlogs/statistics.css':
-            '<%= src %>sass/plugins/redmine_backlogs/statistics.scss',
-          'plugins/redmine_backlogs/taskboard.css':
-            '<%= src %>sass/plugins/redmine_backlogs/taskboard.scss',
-          'plugins/redmine_backlogs/jquery/jquery-ui.css':
-            '<%= src %>sass/plugins/redmine_backlogs/jquery/jquery-ui.scss',
-          'plugins/redmine_backlogs/jquery/jquery.multiselect.css':
-            '<%= src %>sass/plugins/redmine_backlogs/jquery/jquery.multiselect.scss',
-          'plugins/redmine_backlogs/jquery/jquery.qtip.css':
-            '<%= src %>sass/plugins/redmine_backlogs/jquery/jquery.qtip.scss'
-        }
+
+      plugins: {
+        files: [
+          {
+            expand: true,
+            cwd: '<%= src %>sass/plugins/',
+            src: '**/*.scss',
+            dest: 'plugins/',
+            ext: '.css',
+            extDot: 'last'
+          }
+        ]
+      }
+    },
+
+    postcss: {
+      options: {
+        processors: [
+          require('autoprefixer')({ browsers: 'last 2 versions' })
+        ]
+      },
+
+      all: {
+        src: [
+          'stylesheets/*.css',
+          'plugins/**/*.css'
+        ]
       }
     },
 
     uglify: {
-      build: {
+      theme: {
         src: [
           '<%= src %>javascripts/modules/*.js',
           '<%= src %>javascripts/theme.js'
@@ -48,6 +58,7 @@ module.exports = function(grunt) {
         files: ['<%= src %>sass/**/*.scss'],
         tasks: ['css']
       },
+
       js: {
         files: ['<%= src %>javascripts/**/*.js'],
         tasks: ['js']
@@ -56,10 +67,11 @@ module.exports = function(grunt) {
   });
 
   grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-postcss');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks("grunt-contrib-uglify");
 
-  grunt.registerTask('css', ['sass']);
+  grunt.registerTask('css', ['sass', 'postcss']);
   grunt.registerTask('js', ['uglify']);
 
   grunt.registerTask('default', ['css', 'js']);
