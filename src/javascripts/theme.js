9069fa54b8c60cb39c020dcef4d849afb6deb243
Mihai Kelemen <mihaikelemen@gmail.com>
2018-03-24 12:01:46 +0200

    Added Romanian & French translation support. External links are now opening in a new tab

--- src/javascripts/theme.js
+++ src/javascripts/theme.js
@@ -2,6 +2,11 @@ $(function () {
   /* global PurpleMine */
   'use strict'
 
+  	// all external links to open in a new tab
+	$(document.links).filter(function() {
+	    return this.hostname != window.location.hostname;
+	}).attr('target', '_blank');
+
   /* eslint-disable no-new */
   new PurpleMine.SidebarToggler()
   new PurpleMine.HistoryTabs()
